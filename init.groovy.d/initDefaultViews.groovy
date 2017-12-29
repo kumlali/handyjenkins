cafcb2ebfc928e8d0f9f8decde14ac33b7b92e97
kumlali <as_kumlali@yahoo.com>
2017-12-29 16:14:26 +0300

    Make creation of HJ view optional.

--- init.groovy.d/initDefaultViews.groovy
+++ init.groovy.d/initDefaultViews.groovy
@@ -2,17 +2,19 @@ import jenkins.model.*
 import hudson.model.ListView
 
 // Variables
+def env = System.getenv ()
 
 // Constants
-def mngJobs = ["do-swarm-cleanup", "print-installed-plugins"]
-def testJobs = ["color-test",
-                "docker-image-inside-test", 
-                "docker-test",
-                "flyway-test",
-                "git-checkout-test", 
-                "maven-test", 
-                "remote-file-loader-plugin-test",
-                "ssh-test"]
+def hjJobs = ["color-test",
+              "docker-image-inside-test", 
+              "docker-test",
+              "flyway-test",
+              "git-checkout-test", 
+              "maven-test", 
+              "remote-file-loader-plugin-test",
+              "ssh-test",
+              "do-swarm-cleanup", 
+              "print-installed-plugins"]
 
 
 def addView (def viewName, def jobs) {
@@ -37,6 +39,10 @@ def addView (def viewName, def jobs) {
 }
 
 Thread.start {
-  addView ("[HJ] - Setup Tests", testJobs)
-  addView ("[HJ] - Management", mngJobs)  
+  println "[HJ] --> Creating default view..."
+  if (env['HJ_CREATE_DEFAULT_VIEW']=="true") {
+    addView ("[HJ]", hjJobs)
+  } else {
+    println "[HJ] --> Default view was not created because HJ_CREATE_DEFAULT_VIEW is not true."    
+  }
 }
